40534d317e20b4a655a0caaabc528777ea5e4bba
Madness <jonathan.terrettaz@gmail.com>
2023-06-12 14:44:27 +0200

    clean_start

--- srcs/main.c
+++ srcs/main.c
@@ -11,24 +11,9 @@
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
-#include <fcntl.h>
 
-int	main(void)
+int	main(int argc, char **argv, char **env)
 {
-	char	*str;
-	int		fd;
-	int		i;
 
-	i = 0;
-	printf("GNL tester\n");
-	fd = open("./ascii_art/hibou_txt", O_RDONLY);
-	while (i < 8)
-	{
-		str = get_next_line(fd);
-		printf("%s", str);
-		i++;
-	}
-	printf("\n\nft_printf tester\n\n");
-	ft_printf("Coucou hibou \nThe GNL tester printf a total of %d lines\n", i);
 	return (0);
 }
