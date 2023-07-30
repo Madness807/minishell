a5f1e22ecdd38419c184c4764710b2daec3bc598
Hibou42 <hibou42@protonmail.com>
2023-07-30 17:14:44 +0200

    gestion des quote ok, WARNING pas de demande si quote ouverte

--- srcs/lexer/handle_quote.c
+++ srcs/lexer/handle_quote.c
@@ -12,19 +12,62 @@
 
 #include "../../include/minishell.h"
 
+int		update_i_user(int i, char quote, char *str)
+{
+	while (str[i])
+	{
+		if (str[i] == quote)
+		{
+			return (i);
+		}
+		i++;
+	}
+	return (i);
+}
+
 void	handle_quote(t_ms *ms)
 {
-	int	quote_impair;
+	int		i_user;
+	int		i_user_end;
+	t_token	*tmp;
+	t_token	*tmp_end;
+	t_token	*tmp_of_tmp;
+	char	quote;
 
-	if ((ms->info_user->nb_SQ % 2) != 0 || (ms->info_user->nb_DQ % 2) != 0)
-		quote_impair = 1;
-	else
-		quote_impair = 0;
+	i_user = 0;
+	tmp = ms->token;
 	if (ms->info_user->nb_SQ != 0 || ms->info_user->nb_DQ != 0)
 	{
-		if (quote_impair == 1)
+		/*
+		if (is_closed(ms) == 1)
 		{
-			printf("Demande nouvelle ligne pour completage\n");
+			//demande ligne user
+		}
+		*/
+		while (tmp)
+		{
+			if (ft_strcmp(tmp->contenue, "'") == 0 || ft_strcmp(tmp->contenue, "\"") == 0)
+			{
+				quote = tmp->contenue[0];
+				i_user = update_i_user(i_user, quote, ms->user_cmd) + 1;
+				i_user_end = update_i_user(i_user, quote, ms->user_cmd);
+				free(tmp->contenue);
+				tmp->contenue = ft_substr(ms->user_cmd, i_user, i_user_end - i_user);
+				fill_node_tokeniser(tmp->contenue, ms, tmp);
+				tmp_end = tmp->next;
+				while (tmp_end->contenue[0] != quote)
+				{
+					free(tmp_end->contenue);
+					tmp_end->contenue = NULL;
+					tmp_of_tmp = tmp_end->next;
+					free(tmp_end);
+					tmp_end = tmp_of_tmp;
+				}
+				tmp->next = tmp_end->next;
+				i_user = i_user_end + 1;
+			}
+			else
+				tmp = tmp->next;
 		}
 	}
 }
