8fd4045e3407aa7e59cab9e0ebc1d1bca2fbe71d
Hibou42 <hibou42@protonmail.com>
2023-07-31 05:53:55 +0200

    try quote ouvert, je suis pas loin

--- srcs/lexer/handle_quote.c
+++ srcs/lexer/handle_quote.c
@@ -12,6 +12,57 @@
 
 #include "../../include/minishell.h"
 
+void	ask_more(t_ms *ms)
+{
+	char	*tmp;
+	char	*tmp2;
+	char	*user_input;
+
+	user_input = readline("quote> ");
+	tmp2 = ft_strjoin(ms->user_cmd, "\n");
+	if (user_input[0] == '\0')
+	{
+		free(ms->user_cmd);
+		ms->user_cmd = tmp2;
+	}
+	else
+	{
+		tmp = ft_strjoin(tmp2, user_input);
+		free(ms->user_cmd);
+		free(tmp2);
+		ms->user_cmd = tmp;
+	}
+}
+
+void	is_closed(t_ms *ms)
+{
+	char	quote;
+	int 	i;
+	int		stop;
+
+	i = 0;
+	while (ms->user_cmd[i])
+	{
+		if(ms->user_cmd[i] == '\'' || ms->user_cmd[i] == '\"')
+		{
+			quote = ms->user_cmd[i];
+			stop = 0;
+			while (stop == 0)
+			{
+				i++;
+				if (ms->user_cmd[i] == quote)
+					stop = 1;
+				if (ms->user_cmd[i] == '\0')
+				{
+					ask_more(ms);
+					i--;
+				}
+			}
+		}
+		i++;
+	}
+}
+
 int		update_i_user(int i, char quote, char *str)
 {
 	while (str[i])
@@ -38,12 +89,8 @@ void	handle_quote(t_ms *ms)
 	tmp = ms->token;
 	if (ms->info_user->nb_SQ != 0 || ms->info_user->nb_DQ != 0)
 	{
-		/*
-		if (is_closed(ms) == 1)
-		{
-			//demande ligne user
-		}
-		*/
+		is_closed(ms);
+		printf("user input :%s\n", ms->user_cmd);
 		while (tmp)
 		{
 			if (ft_strcmp(tmp->contenue, "'") == 0 || ft_strcmp(tmp->contenue, "\"") == 0)
