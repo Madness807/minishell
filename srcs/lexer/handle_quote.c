2108bd0fa9315a34b83fd3eefc3af4bf5adbea96
Hibou42 <hibou42@protonmail.com>
2023-08-06 21:44:14 +0200

    update espace bug

--- srcs/lexer/handle_quote.c
+++ srcs/lexer/handle_quote.c
@@ -29,17 +29,20 @@ void handle_quote(t_ms *ms)
     {
         if (state == NONE && (*p == ' ' || *p == '\0')) 
         {
-            buffer[buffer_idx] = '\0';
-            t_token *new_token = (t_token *)malloc(sizeof(t_token));
-            new_token->contenue = ft_strdup(buffer);
-            new_token->next = NULL;
-            new_token->previous = last_token;
-            if (last_token) 
-                last_token->next = new_token;
-            else 
-                ms->token = new_token;
-            last_token = new_token;
-            buffer_idx = 0;
+			if (buffer_idx > 0)
+			{
+            	buffer[buffer_idx] = '\0';
+            	t_token *new_token = (t_token *)malloc(sizeof(t_token));
+            	new_token->contenue = ft_strdup(buffer);
+            	new_token->next = NULL;
+            	new_token->previous = last_token;
+            	if (last_token) 
+                	last_token->next = new_token;
+            	else 
+               		ms->token = new_token;
+            	last_token = new_token;
+            	buffer_idx = 0;
+			}
             if (*p == '\0') 
                 break;
         } 
