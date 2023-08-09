7fe8489151d5851962684cfa25963c8c968c2eab
Hibou42 <hibou42@protonmail.com>
2023-08-09 23:12:12 +0200

    update echo

--- srcs/builtins/builtins_echo.c
+++ srcs/builtins/builtins_echo.c
@@ -19,22 +19,25 @@ void	builtin_echo(t_command *command)
 
 	bck_slh_n = 0;
 	i = 0;
-	while (command->tab_options[i])
+	if (command->tab_options)
 	{
-		if (ft_strcmp(command->tab_options[i], "-n") == 0)
+		while (command->tab_options[i])
 		{
-			bck_slh_n = 1;
-			i++;
+			if (ft_strcmp(command->tab_options[i], "-n") == 0)
+			{
+				bck_slh_n = 1;
+				i++;
+			}
+			else
+				break ;
 		}
-		else
-			break ;
-	}
-	while (command->tab_options[i])
-	{
-		ft_printf("%s", command->tab_options[i]);
-		i++;
-		if (command->tab_options[i])
-			ft_printf(" ", command->tab_options[i]);
+		while (command->tab_options[i])
+		{
+			ft_printf("%s", command->tab_options[i]);
+			i++;
+			if (command->tab_options[i])
+				ft_printf(" ", command->tab_options[i]);
+		}
 	}
 	if (bck_slh_n == 0)
 		ft_printf("\n");
